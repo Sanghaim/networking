a32c9e903b76376a11e471fc5a464690b295dfc9
Straka V <vladimir.strak@gmail.com>
2023-12-11 21:08:47 +0100

    Packet

--- src/Packet.ts
+++ src/Packet.ts
@@ -1,8 +1,32 @@
 export default class Packet {
-  _senderHostName?: string;
-  _senderAddress?: string;
-  _receiverHostName?: string;
-  _receiverAddress?: string;
+  private _senderName?: string;
+  private _senderAddress?: string;
+  private _receiverName?: string;
+  private _receiverAddress?: string;
 
-  // https://stackoverflow.com/questions/70521542/typescript-check-multiple-variable-for-undefined
+  constructor(
+    senderName: string,
+    senderAddress: string,
+    receiverName: string,
+    receiverAddress: string
+  ) {
+    if (
+      !checkForNulls([senderName, senderAddress, receiverName, receiverAddress])
+    ) {
+      throw new Error("Packet cannot be empty");
+    } else if (!checkForNulls([senderName, senderAddress])) {
+      throw new Error("Packet needs to have sender");
+    } else if (!checkForNulls([receiverName, receiverAddress])) {
+      throw new Error("Packet needs to have receiver");
+    }
+
+    this._senderAddress = senderAddress;
+    this._senderName = senderName;
+    this._receiverAddress = receiverAddress;
+    this._receiverName = receiverName;
+  }
+}
+
+function checkForNulls<T>(args: (undefined | T)[]): args is T[] {
+  return args.every((e) => e !== undefined);
 }
